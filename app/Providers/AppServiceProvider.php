11e777640a3e3e85ee27ac02903d1191a9adb837
Tazakka Atthariq <145776689+z-yxe@users.noreply.github.com>
2025-06-04 20:54:57 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        
     }
 
     /**
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('layouts.app', function ($view) {
+            $allUsers = User::all();
+            $randomUsersForSidebar = $allUsers->shuffle()->take(10);
+            $view->with('randomUsersForSidebar', $randomUsersForSidebar);
+        });
     }
 }
