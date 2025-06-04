1cfc83a16c64ff9782eabcaf5ac303db3846eef7
Tazakka Atthariq <145776689+z-yxe@users.noreply.github.com>
2025-06-04 14:03:08 +0700

    Delete vendor directory

--- vendor/cloudinary-labs/cloudinary-laravel/views/components/video.blade.php
+++
@@ -1,8 +0,0 @@
-@php
-  $defaultFormatMethod = 'scale';
-  $retrieveFormattedVideo = cloudinary()
-      ->videoTag($publicId ?? '')
-      ->setAttributes(['controls', 'loop', 'preload'])
-      ->fallback('Your browser does not support HTML5 video tagsssss.')
-      ->$defaultFormatMethod($width ?? '', $height ?? '');
-@endphp
