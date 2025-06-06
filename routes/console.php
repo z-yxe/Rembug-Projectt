c2ebe45164d4eb767f73de7b4e6db3a8a555ba4e
Tazakka Atthariq <145776689+z-yxe@users.noreply.github.com>
2025-06-06 18:53:14 +0700

    Update Documentation

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+// Test Artisan commands
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
